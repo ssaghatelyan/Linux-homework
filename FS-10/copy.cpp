0398b1f820e4880268733da891fb0ef61a271afc
susanna <saghatelyansusanna06@gmail.com>
2025-11-09 19:06:22 +0400

    fs-10

--- FS-10/copy.cpp
+++ FS-10/copy.cpp
@@ -28,49 +28,31 @@ int main(int argc, char* argv[]) {
 	ssize_t data_bytes = 0;
 	ssize_t hole_bytes = 0;
 
-	while ((bytes = read(src, buf, sizeof(buf))) > 0){
-		ssize_t i = 0;
-		while(i < bytes){
-			if(buf[i] != 0) {
-				ssize_t  j = i;
-				while (j < bytes && buf[j] != 0) {
-					j++;
+	while ((bytes = read(src, buf, sizeof(buf))) > 0) {
+		for (ssize_t i = 0; i < bytes;) {
+        		if (buf[i] == 0) {
+                	        ssize_t start = i;
+                		while (i < bytes && buf[i] == 0){
+                   			 i++;
 				}
-				ssize_t written = write(dst, buf + i, j - i);
-            			if (written != j - i) { 
-					std::cerr << "Error writing\n";
-					exit(1);
+                		lseek(dst, i - start, SEEK_CUR);
+                		hole_bytes += i - start;
+           		 } else {
+          	               ssize_t start = i;
+                		while (i < bytes && buf[i] != 0){
+                   			 i++;
 				}
-				data_bytes += j - i;
-				i = j;
-			}
-			else{
-				ssize_t j = i;
-				while (j < bytes && buf[j] == 0) {
-                                        j++;
-                                }
-                                if (lseek(dst, j - i, SEEK_CUR) == -1) {
-					std::cerr << "Error with lseek\n";
-				       	exit(1);
-			       	}
-				hole_bytes += j - i;
-                                i = j;
-			}
-		}
-	}
-	if(bytes == -1) {
-		std::cerr << "Error reading source file\n";
-		exit(1);
-	}
+               		 write(dst, buf + start, i - start);
+                	data_bytes += i - start;
+            }
+        }
+    }
 
 	close(src);
 	close(dst);
 
-	if(bytes == -1) {
-		std::cerr << "Error reading file\n";
-		exit(1);
-	}
+	ssize_t total = data_bytes + hole_bytes;
 
-	std::cout << "Successfuly copied: " << (data_bytes + hole_bytes) << " bytes: \ndata: " << data_bytes << ", hole: " << hole_bytes << "\n";
+	std::cout << "Successfuly copied: " << total  << " bytes: \ndata: " << data_bytes << ", hole: " << hole_bytes << "\n";
 
 }
